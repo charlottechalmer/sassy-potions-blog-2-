eed549a688a207de37a985b896f564094663c615
charlottechalmer <charlotte.taylor7693@gmail.com>
2016-11-02 19:52:26 -0600

    Sassy Potions

---
+++ SassyPotion.js
@@ -0,0 +1,17 @@
+/* jshint esversion:6 */
+module.exports = function(mongoose, SassyComment) {
+
+	var SassyPotionSchema = new mongoose.Schema({
+		sass: Number,
+		ingredients: [String],
+		crystals: Boolean,
+		name: String,
+		gpa: Number
+		// _id: ObjectId
+		// comments: [SassyComment] // subdocument
+	});
+
+	var SassyPotion = mongoose.model("SassyPotion", SassyPotionSchema);
+
+	return SassyPotion;
+};
